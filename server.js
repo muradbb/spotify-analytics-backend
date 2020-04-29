33d57b94f000b80aa5e72b36324dc8f5355d6c53
ipl4dc <ipl4dc@inf.elte.hu>
2020-04-29 23:59:53 +0200

    Guess what? Scopes :(

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.get('/login', function(req, res) {
     querystring.stringify({
       response_type: 'code',
       client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: 'user-read-private user-read-email user-top-read playlist-modify-private playlist-modify-public',
+      scope: 'user-read-private user-read-email user-top-read '+
+      'playlist-modify-private playlist-modify-public user-read-recently-played',
       redirect_uri
     }))
 })
